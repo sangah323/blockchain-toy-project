68fe6898aa4a7e3eca5638c61fa364ecfa6405c1
sangah <sangah@Testui-MacBookAir.local>
2025-05-26 20:55:25 +0900

    fix : STKToken.sol 수정

--- blockchain/contracts/STKToken.sol
+++ blockchain/contracts/STKToken.sol
@@ -2,32 +2,16 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract STKToken is ERC20 {
-    address public owner;
-
+contract STKToken is ERC20, Ownable {
     constructor(
         string memory _name,
         string memory _symbol
-    ) ERC20(_name, _symbol) {
-        owner = msg.sender;
-    }
+    ) ERC20(_name, _symbol) Ownable(msg.sender) {} // Ownable(msg.sender): 초기 배포자
 
     // 보상용 토큰 발행 (owner only)
-    function minting() public {
-        require(msg.sender == owner, "Only owner can mint");
-        _mint(owner, 1_000_000 * 10 ** decimals()); // owner(), 컨트랙트 배포자에게 100만 토큰 민팅
-    }
-
-    // owner가 사용자에게 보상 지급
-    function reward(address user, uint256 amount) public {
-        require(msg.sender == owner, "Only owner can mint");
-        require(balanceOf(owner) >= amount, "Not enough balance");
-        _transfer(owner, user, amount); // owner가 user에게 amount 지급
-    }
-
-    // 토큰 소유자 주소 조회
-    function getOwner() public view returns (address) {
-        return owner;
+    function mintSTK() public onlyOwner {
+        _mint(owner(), 1_000_000 * 10 ** decimals()); // owner(), 컨트랙트 배포자에게 100만 토큰 민팅
     }
 }
